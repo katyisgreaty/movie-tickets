01a23d08d873c00bc390d3a32d7bb3c7402eeb5e
Katy Daviscourt and Shruti Priya <katy@>
2017-01-24 14:57:55 -0800

    add small changes in user logic and business logic to the scripts file which make multiple discounts to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,57 @@
 // Business logic goes here.....
-var movieArray = ["La La Land", "Hidden Figures", "The Hobbit", "Gone with the Wind"]
+var movieArray = ["La La Land", "Hidden Figures", "The Hobbit", "Gone with the Wind"];
+var timeArray = ["11:30 am (Matinee)", "3:00 pm (Matinee)"];
 
-var ticketPrice = 25;
-
-var Ticket = function(movie, number, time, userAge){
+var Ticket = function(name, email, movie, number, time, userAge){
+  this.selectedName = name;
+  this.selectedEmail = email;
   this.selectedMovie = movie;
   this.selectedNumber = number;
   this.selectedTime = time;
   this.selectedUserAge = userAge;
-  this.price;
+  this.price = 25;
 };
 
 Ticket.prototype.classicDiscount = function() {
   if((this.selectedMovie === movieArray[0]) || (this.selectedMovie === movieArray[1])) {
-    this.price = ticketPrice;
-    return (this.price);
   } else {
-    this.price = ticketPrice - 5;
-    // return (ticketPrice - 5);
-    return (this.price);
+    this.price -= 5;
+    console.log("It's a classic!");
   }
 }
+  Ticket.prototype.matineeDiscount = function() {
+  if((this.selectedTime === timeArray[0]) || (this.selectedTime === timeArray[1])) {
+    this.price -= 5;
+    console.log("it's a matinee!");
+  } else {
+  }
+}
+
 
 // User logic goes here ....
 $(document).ready(function(){
   $("form#tickets").submit(function(event){
     event.preventDefault();
 
+    var inputtedName = $("input#name").val();
+    var inputtedEmail = $("input#email").val();
     var inputtedMovie = $("select#movie").val();
     var inputtedNumber = $("input#number").val();
     var inputtedTime = $("select#time").val();
     var inputtedUserAge = $("select#age").val();
-    var myTicket = new Ticket(inputtedMovie, inputtedNumber, inputtedTime, inputtedUserAge);
+    var myTicket = new Ticket(inputtedName, inputtedEmail, inputtedMovie, inputtedNumber, inputtedTime, inputtedUserAge);
 
-    var infoOutput = ("Your movie: " + myTicket.selectedMovie + "<li>" + " Number of tickets booked: " +  myTicket.selectedNumber + "</li><li>" + " Showtime: " +myTicket.selectedTime + "</li><li>" + " Ticket type: " + myTicket.selectedUserAge);
+    myTicket.classicDiscount();
+    myTicket.matineeDiscount();
 
-    var totalPrice = "Your total price is $" + (myTicket.classicDiscount() * ($("input#number").val())) + ".00";
+    var infoOutput = ("Your Name: " + myTicket.selectedName +
+                      "<li>" + "Your email: " + myTicket.selectedEmail +
+                      "</li><li>" + "Your movie: " + myTicket.selectedMovie +
+                      "</li><li>" + " Number of tickets booked: " +  myTicket.selectedNumber +
+                      "</li><li>" + " Showtime: " +myTicket.selectedTime +
+                      "</li><li>" + " Ticket type: " + myTicket.selectedUserAge);
+
+    var totalPrice = "Your total price is $" + (myTicket.price * ($("input#number").val())) + ".00";
 
   $("span.output").last().append("<li>" + infoOutput + "</li><li>" + totalPrice + "</li>");
 
